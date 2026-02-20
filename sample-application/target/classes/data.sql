1baddcfb9c2db7efcbc00aedb61b3e38ede16063
Daniel Kpobari <daniel.baghadom@wizerconsulting.com>
2026-02-20 01:24:52 +0100

    integration tests fixed

--- sample-application/target/classes/data.sql
+++ sample-application/target/classes/data.sql
@@ -1,7 +1,7 @@
 -- Password for all users is: password123
--- BCrypt hash generated with strength 10
+-- BCrypt hash: $2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6
 
 INSERT INTO users (username, password, roles) VALUES 
-('john', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', 'ROLE_USER'),
-('admin', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', 'ROLE_USER,ROLE_ADMIN'),
-('jane', '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy', 'ROLE_USER');
+('john', '$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6', 'ROLE_USER'),
+('admin', '$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6', 'ROLE_USER,ROLE_ADMIN'),
+('jane', '$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6', 'ROLE_USER');
